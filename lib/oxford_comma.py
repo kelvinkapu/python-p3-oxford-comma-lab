652d523b387acd6f694f320587a878f1ac2050ce
Kelvin Kapaya <kalvinkapu@gmail.com>
2023-08-21 21:53:40 +0300

    Commas

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+#     return " ".join(items)
+# def oxford_comma(items):
+#     return " and ".join(items)
+# def oxford_comma(items):
+    if len(items) > 1:
+        items[-1] = "and " + items[-1]
+
+    if len(items) > 2:
+        return ', '.join(items)
+    else:
+        return ' '.join(items)
+
+print(oxford_comma(["kiwi", "durian", "starfruit"]))
